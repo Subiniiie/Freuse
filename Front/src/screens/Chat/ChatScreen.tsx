fec4515ff7dffec9ea0cb4e6d616456260a8e6c5
Subiniiie <subin57419@gmail.com>
2024-12-12 22:16:38 +0900

    chat 배경색 설정

--- Front/src/screens/Chat/ChatScreen.tsx
+++ Front/src/screens/Chat/ChatScreen.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, SafeAreaView, Text } from 'react-native'
 import React from 'react'
 
 export default function ChatScreen() {
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>ChatScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+})
