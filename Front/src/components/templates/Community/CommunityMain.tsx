99a3b7ba943c7b40585c898b5ec25dd180efe624
Subiniiie <subin57419@gmail.com>
2024-12-12 22:40:07 +0900

    글쓰기 버튼 생성

--- Front/src/components/templates/Community/CommunityMain.tsx
+++ Front/src/components/templates/Community/CommunityMain.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { ScrollView } from 'react-native'
 import CommunityStyles from '../../../styles/Community/CommunityStyles'
 import FilterBar from '../../molecules/Community/FilterBar'
-
+import WriteBtn from '../../atoms/Community/WriteBtn'
 
 export default function CommunityMain() {
   return (
-    <ScrollView contentContainerStyle={CommunityStyles.contentContainer}>
-        <FilterBar />
-    </ScrollView>
+    <>
+      <ScrollView contentContainerStyle={CommunityStyles.contentContainer}>
+          <FilterBar />
+      </ScrollView>
+      {/* 화면에 고정되어야 함 -> 스크롤 되면 안됨 -> ScrollView 밖에 위치 */}
+      <WriteBtn />
+    </>
   )
 }
