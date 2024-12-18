bfbe961a5cc480d3a3b5c1e1116b74787a625728
Subiniiie <subin57419@gmail.com>
2024-12-18 21:31:41 +0900

    커밋 취소

--- Front/App.tsx
+++ Front/App.tsx
@@ -5,18 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigates/AppNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 
 
 function App(): React.JSX.Element {
+
+  
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
--- Front/src/navigates/AppNavigator.tsx
+++ Front/src/navigates/AppNavigator.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabNavigator from "./BottomTabNavigator";
+import SplashMainScreen from "../screens/Splash/SplashMainScreen";
+
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
     return (
         <Stack.Navigator>
+            <Stack.Screen
+                name="Splash"
+                component={SplashMainScreen}
+                options={{headerShown: false}}
+            />
             <Stack.Screen 
                 name="Main"
                 component={BottomTabNavigator}
