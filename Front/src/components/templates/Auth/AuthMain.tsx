835f7b99952190337096f7997fb2da475850a306
Subiniiie <subin57419@gmail.com>
2024-12-23 13:03:24 +0900

    Auth 메인 페이지 생성

---
+++ Front/src/components/templates/Auth/AuthMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from '@rneui/themed'
+import AuthBtns from '../../molecules/Auth/AuthBtns'
+
+export default function AuthMain() {
+  return (
+    <>
+        <Text h1>프리유즈</Text>
+        <AuthBtns />
+    </>
+  )
+}
--- Front/src/screens/Auth/AuthScreen.tsx
+++ Front/src/screens/Auth/AuthScreen.tsx
@@ -1,10 +1,8 @@
-import { Text, View } from 'react-native'
 import React from 'react'
+import AuthMain from '../../components/templates/Auth/AuthMain'
 
 export default function AuthScreen() {
   return (
-    <View>
-      <Text>AuthScreen</Text>
-    </View>
+    <AuthMain />
   )
 }
