1eeca525325d89225b69c0fcff1bc976c0598897
Subiniiie <subin57419@gmail.com>
2024-12-30 12:44:53 +0900

    백엔드 DB 연결 중

---
+++ Back/src/test/java/com/freuse/freuse/FreuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freuse.freuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
