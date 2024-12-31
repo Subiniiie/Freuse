44950ca4e769e792730b8181a35d58c91b2e455a
Subiniiie <subin57419@gmail.com>
2024-12-31 14:22:22 +0900

    회원가입 서비스 생성

--- Back/src/main/java/com/freuse/freuse/domain/user/service/UserServiceImpl.java
+++ Back/src/main/java/com/freuse/freuse/domain/user/service/UserServiceImpl.java
@@ -4,27 +4,34 @@ package com.freuse.freuse.domain.user.service;
 import com.freuse.freuse.domain.user.entity.User;
 import com.freuse.freuse.domain.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(String username, String password, String email) {
+        if (userRepository.existsByUsername(username)) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일 입니다.");
+        }
         User user = new User();
 
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         user.setEmail(email);
-
-
 
         return userRepository.save(user);
     }
