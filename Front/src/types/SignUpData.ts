f88ae59d9e4e692693f6a50ecd50875eb5585d58
Subiniiie <subin57419@gmail.com>
2025-01-15 22:43:18 +0900

    회원가입 api 연동 중

--- Front/src/types/SignUpData.ts
+++ Front/src/types/SignUpData.ts
@@ -6,6 +6,12 @@ export interface SignUpData {
 export interface SignUpFormData {
     username: string;
     password: string;
+    email: string;
+}
+
+export interface SignUpFormDataCheck {
+    username: string;
+    password: string;
     confirmPassword: string;
     email: string;
 }
