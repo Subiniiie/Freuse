9dc1b17783d18212c175d5156828762f764a6bdb
Subiniiie <subin57419@gmail.com>
2025-01-18 13:29:10 +0900

    회원가입 form data 참조값 수정

--- Front/src/components/molecules/Auth/SignUpInputs.tsx
+++ Front/src/components/molecules/Auth/SignUpInputs.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import SignUpInput from '../../atoms/Auth/SignUpInput'
 import signUpData from '../../../data/Auth/SignUpData'
-import useSignup from '../../../hooks/Auth/Signup/useSignup';
-import { SignUpFormData } from '../../../types/SignUpData';
+import { SignUpFormDataCheck } from '../../../types/SignUpData';
 
 type SignUpInputsProps = {
-  handleChange: (field: keyof SignUpFormData) => (text: string) => void;
+  handleChange: (field: keyof SignUpFormDataCheck) => (text: string) => void;
   getValueIndex: (index: number) => string;
-  getFieldIndex: (index: number) => keyof SignUpFormData;
+  getFieldIndex: (index: number) => keyof SignUpFormDataCheck;
 }
 
 export default function SignUpInputs({ handleChange, getValueIndex, getFieldIndex}: SignUpInputsProps) {
