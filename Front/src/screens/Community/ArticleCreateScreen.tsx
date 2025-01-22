50eb6832d6a72dc998e2173e3eb6a169d04e1da9
Subiniiie <subin57419@gmail.com>
2025-01-22 13:35:25 +0900

    거래 글쓰기 화면 생성

---
+++ Front/src/screens/Community/ArticleCreateScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function ArticleCreateScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+})
