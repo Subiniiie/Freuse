a6bebc16baeb909fee610159a996d1fb3fcc19c8
박수빈 <subin57419@gmail.com>
2025-01-28 12:15:03 +0900

    community dto 생성

---
+++ Back/src/main/java/com/freuse/freuse/domain/community/CommunityDto.java
@@ -0,0 +1,13 @@
+package com.freuse.freuse.domain.community;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommunityDto {
+
+    private String title;
+    private String content;
+}
